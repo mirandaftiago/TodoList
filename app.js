e6472930a4c5255b0ea304b7a5615b5f61f60147
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-14 16:29:47 +0000

    create removeTaskFromLocalStorage()

--- app.js
+++ app.js
@@ -100,9 +100,28 @@ function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')) {
     if(confirm('Are you sure?')) {
       e.target.parentElement.parentElement.remove();
-    }
 
+      //Remove task from LocalStorage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+    }
   }  
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index) {
+    if(taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 //Clear tasks
